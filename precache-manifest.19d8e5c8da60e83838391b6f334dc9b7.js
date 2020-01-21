81a532ed85a5010fa6ed74f8e2e9a974deb77d19
rahul_nurtr <rahul@nurtr.com>
2020-01-21 10:06:16 +0530

    Updates

---
+++ precache-manifest.19d8e5c8da60e83838391b6f334dc9b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46ff1a7237d7a61de15de61669f049bb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "352de531490b8bbda2d2",
+    "url": "/monsters-rolodex/static/css/main.7963a2bb.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "352de531490b8bbda2d2",
+    "url": "/monsters-rolodex/static/js/main.e012dadb.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
